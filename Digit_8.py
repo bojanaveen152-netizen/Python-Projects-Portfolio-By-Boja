863dc0e5c579246b195e9a1cabcd1966ceb5f4d6
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-08-25 06:19:09 +0100

    Create Digit_8.py

---
+++ Digit_8.py
@@ -0,0 +1,14 @@
+a=int(input())
+for i in range(1,a+1):
+    if i == 1:
+        print("* "*a)
+    else:
+        b=(a-2)
+        print("* "+"  "*b+"* ") 
+for i in range(1,a+1):
+    if i == 1:
+        print("* "*a)
+    else:
+        b=(a-2)
+        print("* "+"  "*b+"* ")
+print("* "*a)
