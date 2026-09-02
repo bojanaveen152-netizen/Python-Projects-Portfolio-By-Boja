8ef79335d7da0dd6807a2e3e9fffc0e6282a7e92
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-09-02 07:20:12 +0100

    Create List_Indexing_2.py

---
+++ List_Indexing_2.py
@@ -0,0 +1,6 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+number=int(input())
+for i in range(1,number+1):
+    s_input=int(input())
+    index=programming_languages_list[s_input]
+    print(index)
