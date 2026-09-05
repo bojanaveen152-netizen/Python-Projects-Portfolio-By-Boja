b2831d9aa2365fd9aefb28346144f22772d95c42
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-09-05 08:21:14 +0100

    Create Second_Character_in_Word.py

---
+++ Second_Character_in_Word.py
@@ -0,0 +1,9 @@
+def second_character(arg_1):
+    # Complete this function
+    result=(arg_1[1])
+    return result
+
+word = input()
+# Call the second_character function
+result=second_character(arg_1=word)
+print(result)
