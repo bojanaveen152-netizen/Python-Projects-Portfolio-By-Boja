73d885245d5c959ee8c5457bfccb6cafa5244539
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-09-05 07:28:11 +0100

    Create Between_200_and_500.py

---
+++ Between_200_and_500.py
@@ -0,0 +1,10 @@
+def is_between_200_and_500(number):
+    if (number>200 and number<500):
+          print("Yes")
+    else:
+        print("No")
+    
+
+number = int(input())
+#call the is_between_200_and_500 function
+integer=is_between_200_and_500(number)
