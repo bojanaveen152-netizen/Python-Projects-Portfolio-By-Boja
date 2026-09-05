2f55e2721a0a72b5a8fdcf095225c5e7a08d65ef
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-09-05 08:06:40 +0100

    Create Divisible_by_Seven.py

---
+++ Divisible_by_Seven.py
@@ -0,0 +1,8 @@
+def divisible_by_seven(arg_1):
+    # Write your code here
+        if arg_1 % 7==0:
+           print("True")
+        else:
+           print("False")
+n = int(input())
+divisible_by_seven(arg_1=n)
